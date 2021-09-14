23b3744f3731f91c0675beaf1dc1d82f2e92dbff
NV <nikola.vrbinarova@accenture.com>
2021-09-14 15:45:50 +0200

    initial commit

---
+++ src/components/Animation.js
@@ -0,0 +1,73 @@
+import React,{useState,useRef,useEffect} from 'react';
+import './Animation.css';
+import { ImageData } from './Data';
+
+function Animation() {
+
+    const [current,setCurrent] = useState(1);
+    const length = ImageData.length;
+    const timeout= useRef(null);
+
+    const onValueChange = (event) => {  
+       // console.log(event.target.value +" "+ event.target.id);       
+    }
+
+    const nextSlide =(value)=>{
+        if (value!==current)
+        {
+        if(timeout.current){clearTimeout(timeout.current)};
+        setCurrent(value);
+        }
+    }
+    
+    useEffect(
+        ()=>{
+        const nextSlide =()=>
+        {
+        setCurrent(current=>(current===length ? 1: current +1));
+        //console.log(current);
+        };
+        timeout.current = setTimeout(nextSlide,3000);
+        return function(){
+        if(timeout.current){clearTimeout(timeout.current);}
+        };
+        },[current, length]);  
+
+    return (
+        <div className="carousel-wrapper">  
+
+            <div className="legend-img">
+                Selected option is : {current}
+            </div>          
+    
+            <div className="radio-button-wrapper">
+                {ImageData.map((item)=>(
+
+                <div key ={item.id} className="radio-button">
+                    <label>
+                        <input                   
+                        type="radio"
+                        value={item.label}
+                        checked={current === item.id}
+                        onChange={onValueChange}
+                        onClick={()=>nextSlide(item.id)}
+                        />
+                        {/* {item.label} */}
+                    </label>         
+                </div>
+                ))}
+            </div>
+
+            <div className="image-wrapper">
+                {ImageData.map((item)=>(
+                    <div key ={item.id} className={current === item.id ? 'visible':'hidden'}>
+                        <img className="image" src={item.src} alt={item.label}></img>
+                    </div>
+                ))}            
+            </div>  
+                       
+        </div>
+    )
+}
+
+export default Animation
---
+++ src/components/Data.js
@@ -0,0 +1,23 @@
+export const ImageData = [
+
+{
+    id:1,
+    label:'image1',
+    src:'https://randomwordgenerator.com/img/picture-generator/54e1d444424faa0df7c5d57bc32f3e7b1d3ac3e45551754c7c267bd697_640.jpg',
+    alt:'image1'
+},
+{
+    id:2,
+    label:'image2',
+    src:'https://randomwordgenerator.com/img/picture-generator/53e0d34a4c53a814f1dc8460962e33791c3ad6e04e50744172297cd5914cc7_640.jpg',
+    alt:'image2'
+},
+{
+    id:3,
+    label:'image3',
+    src:'https://randomwordgenerator.com/img/picture-generator/55e8d146425bb10ff3d8992cc12c30771037dbf852547848702a7fd19545_640.jpg',
+    alt:'image3'
+},
+
+
+]
